f8bc9c285847f88f1cf03409fd211251b7df1be4
Marina Nieblas <mnieblas@doctorjekyll.com>
2019-08-09 10:08:09 +0200

    Updates

---
+++ precache-manifest.3bb6b4e46dc5c751320a688ef9612d21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652abeb860084645b1e0bae5db8a5b4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dee8f330989f941e2a4",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "4dee8f330989f941e2a4",
+    "url": "/static/js/main.590c8914.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
